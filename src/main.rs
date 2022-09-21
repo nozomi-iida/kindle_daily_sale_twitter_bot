4de85be59586ede5570b1b42beaf368b5827fd53
nozomi-iida <iida19990106@gmail.com>
2022-09-21 09:42:27 +0900

    feat: tweet daily sale

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use chrono::Local;
 use dotenv::dotenv;
+use tokio::task::spawn_blocking;
 
 mod common;
 mod twitter_api;
@@ -11,8 +12,7 @@ async fn main() {
   let books = amazon_crawler::get_daily_sale_books();
   let date = Local::now().format("%Y年%m月").to_string();
   for book in books {
-    let content = format!("{}のKindle日替わりセール\n {}: {}", date, book.title, book.amazon_url);
-    println!("{:?}", content);
-    //   let _ = twitter_api::create_tweet(content).await;
+    let content = format!("{}のKindle日替わりセール\n {}\n {}", date, book.title, book.amazon_url);
+      let _ = twitter_api::create_tweet(content).await;
   }
 }
