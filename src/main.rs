01c4b03439317f0ed7ba0a4e4a27b7e4ec846d1d
nozomi-iida <iida19990106@gmail.com>
2022-10-01 19:31:00 +0900

    feat: delete tweet

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use chrono::Local;
 use dotenv::dotenv;
 
-mod common;
 mod twitter_api;
 mod amazon_crawler;
 
@@ -11,7 +10,8 @@ async fn main() {
   let books = amazon_crawler::get_daily_sale_books();
   let date = Local::now().format("%Y年%m月").to_string();
   for book in books {
-    let content = format!("{}のKindle日替わりセール\n {}\n {}", date, book.title, book.amazon_url);
+    let content = format!("【{}のKindle日替わりセール】\n {}\n {}", date, book.title, book.amazon_url);
       let _ = twitter_api::create_tweet(content).await;
   }
+  // let _ = twitter_api::delete_all_tweet().await;
 }
--- src/twitter_api/mod.rs
+++ src/twitter_api/mod.rs
@@ -1,9 +1,20 @@
+mod auth;
+
+use egg_mode::tweet;
 use egg_mode::tweet::DraftTweet;
-use crate::common;
 
 pub async fn create_tweet(content: String) -> Result<(), Box<dyn std::error::Error>> {
-  let config = common::Config::load().await;
+  let config = auth::Config::load().await;
   let tweet = DraftTweet::new(content.clone());
   tweet.send(&config.token).await?;
   Ok(())
+}
+
+pub async fn delete_all_tweet() {
+  let config = auth::Config::load().await;
+  let tweets = tweet::user_timeline(config.user_id, true, false, &config.token).with_page_size(200);
+  let (_home, feed) = tweets.start().await.unwrap();
+  for status in feed.into_iter() {
+    let _ = tweet::delete(status.response.id, &config.token).await.unwrap();
+  }
 }
